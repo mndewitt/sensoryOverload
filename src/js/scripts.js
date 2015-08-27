b8c506bab3bbacfef8f908117b012c0af16913cd
mndewitt <mdewitt07@gmail.com>
2015-08-26 20:24:23 -0400

    refactor

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,45 +1,54 @@
 (function() {
-	var ctx = new AudioContext(),
-		audio = document.getElementById('track'),
-		audioSrc = ctx.createMediaElementSource(audio),
-		analyser = ctx.createAnalyser(),
-		frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-	audioSrc.connect(analyser);
-	audioSrc.connect(ctx.destination);
+	var freqVisualizer = {
+		init: function() {
+			freqVisualizer.visualizer();
+		},
 
-	//Basic frequency chart
+		visualizer: function() {
+			var ctx = new AudioContext(),
+				audio = document.getElementById('track'),
+				audioSrc = ctx.createMediaElementSource(audio),
+				analyser = ctx.createAnalyser(),
+				frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-	function renderFrame() {
-		var totalNodes = 200;
+			audioSrc.connect(analyser);
+			audioSrc.connect(ctx.destination);
 
-		requestAnimationFrame(renderFrame);
-		analyser.getByteFrequencyData(frequencyData);
-		
-		for(var i = 1; i <= totalNodes; i++) {
-			var nodeId = 'freq-node' + i;
-			setNodeStyles(nodeId, frequencyData, i*2, i);
+			function renderFrame() {
+				var totalNodes = 200;
+
+				requestAnimationFrame(renderFrame);
+				analyser.getByteFrequencyData(frequencyData);
+				
+				for(var i = 1; i <= totalNodes; i++) {
+					var nodeId = 'freq-node' + i;
+					freqVisualizer.setNodeStyles(nodeId, frequencyData, i*2, i);
+				}
+			}
+
+			//Initiate the whole thing
+			audio.play(0);
+			renderFrame();
+		},
+
+		setNodeStyles: function(el, freqArray, spectrum, index) {
+			var el = document.getElementById(el),
+				height = 'height:' + freqArray[spectrum] * 2 + 'px;',
+				left = 'left:' + index * 6 + 'px',
+				style = height + left;
+
+				if(freqArray[spectrum] > 140 ) {
+					el.className = 'freq-node blue';
+				} else if(freqArray[spectrum] > 70 ) {
+					el.className = 'freq-node green';
+				} else {
+					el.className = 'freq-node';
+				}
+
+			el.setAttribute('style', style);
 		}
 	}
 
-	function setNodeStyles(el, freqArray, spectrum, index) {
-		var el = document.getElementById(el),
-			height = 'height:' + freqArray[spectrum] * 2 + 'px;',
-			left = 'left:' + index * 6 + 'px',
-			style = height + left;
-
-			if(freqArray[spectrum] > 140 ) {
-				el.className = 'freq-node blue';
-			} else if(freqArray[spectrum] > 70 ) {
-				el.className = 'freq-node green';
-			} else {
-				el.className = 'freq-node';
-			}
-
-		el.setAttribute('style', style);
-	}
-
-	//Initiate the whole thing
-	audio.play(0);
-	renderFrame();
+	freqVisualizer.init();
 })();

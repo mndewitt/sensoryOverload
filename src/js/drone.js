548f9d73efcec6f162d19e16378e1145dc6631ee
mndewitt <mdewitt07@gmail.com>
2015-08-27 22:08:57 -0400

    display note name on page

--- src/js/drone.js
+++ src/js/drone.js
@@ -13,6 +13,7 @@
 				up = document.getElementById('pitchup'),
 				down = document.getElementById('pitchdown'),
 				isAlreadyPlaying = false,
+				noteArrayIndex = 0,
 				osc;
 
 			//Various click events to control oscillator
@@ -30,21 +31,51 @@
 				osc.connect(ctx.destination);
 				osc.start();
 				isAlreadyPlaying = true;
+				drone.bindCurrentNote(noteArrayIndex);
 			}
 
 			stop.onclick = function() {
 				osc.stop();
 				isAlreadyPlaying = false;
+				drone.bindCurrentNote(false);
 			}
 
 			up.onclick = function() {
 				freq = freq * 1.0595;
 				osc.frequency.value = freq;
+
+				if(noteArrayIndex === 11) {
+					noteArrayIndex = 0;
+				} else {
+					noteArrayIndex++;
+				}
+
+
+				drone.bindCurrentNote(noteArrayIndex);
 			}
 
 			down.onclick = function() {
 				freq = freq / 1.0595;
 				osc.frequency.value = freq;
+
+				if(noteArrayIndex === 0) {
+					noteArrayIndex = 11;
+				} else {
+					noteArrayIndex--;
+				}
+
+				drone.bindCurrentNote(noteArrayIndex);
+			}
+		},
+
+		bindCurrentNote: function(noteArrayIndex) {
+			var noteArray = ['A', 'Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'],
+				noteNameEl = document.getElementById('note-name');
+			
+			if(noteArrayIndex === false) {
+				noteNameEl.innerHTML = '';
+			} else {
+				noteNameEl.innerHTML = noteArray[noteArrayIndex];
 			}
 		}
 	}

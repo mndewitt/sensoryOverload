996081220c12c301448e697a816eb6caef5f6f4c
mndewitt <mdewitt07@gmail.com>
2015-08-27 21:30:56 -0400

    prevent multiple instances of oscillator when pressing start

--- src/js/drone.js
+++ src/js/drone.js
@@ -12,20 +12,29 @@
 				stop = document.getElementById('stopsound'),
 				up = document.getElementById('pitchup'),
 				down = document.getElementById('pitchdown'),
+				isAlreadyPlaying = false,
 				osc;
 
 			//Various click events to control oscillator
 			start.onclick = function() {
+
+				//Prevent multiple oscillators from playing
+				if(isAlreadyPlaying === true) {
+					return;
+				}
+
 				//We need to initiate a new oscillator ever time we start
 				osc = ctx.createOscillator();
 				osc.type = 'sine';
 				osc.frequency.value = freq;
 				osc.connect(ctx.destination);
 				osc.start();
+				isAlreadyPlaying = true;
 			}
 
 			stop.onclick = function() {
 				osc.stop();
+				isAlreadyPlaying = false;
 			}
 
 			up.onclick = function() {

b038f8ae10f1f7cf53def013b9f51f63a8db163c
mndewitt <mdewitt07@gmail.com>
2015-09-21 21:37:22 -0400

    add todo for beat detector

---
+++ src/js/beatdetector.js
@@ -0,0 +1,9 @@
+/*
+Let's make a beat detector with: 
+
+http://tech.beatport.com/2014/web-audio/beat-detection-using-web-audio/
+
+And then do things like change something at every interval (time based
+visualizer instead of frequency based)
+
+*/

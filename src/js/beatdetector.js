ba6ddccc2abd956197009a1bb357b66fdc8805bf
mndewitt <mdewitt07@gmail.com>
2015-09-26 17:47:39 -0400

    some more todos

--- src/js/beatdetector.js
+++ src/js/beatdetector.js
@@ -3,7 +3,10 @@ Let's make a beat detector with:
 
 http://tech.beatport.com/2014/web-audio/beat-detection-using-web-audio/
 
+http://www.techtalkshub.com/bpm-detection-javascript/
+
 And then do things like change something at every interval (time based
 visualizer instead of frequency based)
 
 */
+
